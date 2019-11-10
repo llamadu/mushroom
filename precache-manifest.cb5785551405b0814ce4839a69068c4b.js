2fea53e3659992958ebfe32ed2805afe250a70d5
Scarlett <jenny_lieseke@hotmail.com>
2019-11-09 20:44:09 -0800

    Updates

---
+++ precache-manifest.cb5785551405b0814ce4839a69068c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a178ea96da68c01f5d1be70a754a830",
+    "url": "/mushroom/index.html"
+  },
+  {
+    "revision": "604b5afdce8c43f4c613",
+    "url": "/mushroom/static/css/main.172dd09b.chunk.css"
+  },
+  {
+    "revision": "c63598b07a95846f5e26",
+    "url": "/mushroom/static/js/2.e0ea9233.chunk.js"
+  },
+  {
+    "revision": "604b5afdce8c43f4c613",
+    "url": "/mushroom/static/js/main.885b84c8.chunk.js"
+  },
+  {
+    "revision": "4956169ec04846f4cc81",
+    "url": "/mushroom/static/js/runtime-main.c82f3080.js"
+  }
+]);
--- src/components/Container/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const Container = props => <div className="container">{props.children}</div>;
-
-export default Container;
